c9259f7ed0bb36331bc197d910c83da198f25abf
MickiMouse <495lolkek42@gmail.com>
2020-10-18 16:42:08 +0300

    Initial flask app

--- scripts/fill.sql
+++
@@ -1,4 +0,0 @@
-SET GLOBAL local_infile = 1;
-USE project;
-
-LOAD DATA INFILE '/Education/SQL/Project/scripts/coach.csv' INTO TABLE Coach FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n';
