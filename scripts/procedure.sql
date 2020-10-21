b2b52010c418858a2fa0d481097a9636230d56ed
vladimir <chernvld@gmail.com>
2020-10-21 21:29:10 +0300

    Fuck heroku

--- scripts/procedure.sql
+++ scripts/procedure.sql
@@ -7,7 +7,7 @@ DROP PROCEDURE IF EXISTS selection //
 
 CREATE PROCEDURE selection (IN table_name NCHAR(20))
 BEGIN
-    SELECT * FROM table_name;
+
 END //
 
 -- Удалить запись из заданной таблицы по id
@@ -18,13 +18,23 @@ BEGIN
     DELETE FROM table_name WHERE id = del_id;
 END //
 
+-- Получить столбцы из заданной таблицы
+DROP PROCEDURE IF EXISTS get_rows //
+
+CREATE PROCEDURE get_rows(IN p_table_name NCHAR(20))
+BEGIN
+    SELECT JSON_ARRAYAGG(column_name) FROM information_schema.columns WHERE table_name = p_table_name;
+END //
+
 -- Изменить строку из заданной таблицы
 DROP PROCEDURE IF EXISTS updation //
 
 CREATE PROCEDURE updation (IN p_table_name NCHAR(20), IN up_id INT, IN vals NCHAR(1000))
 BEGIN
-    CREATE VIEW cols AS SELECT column_name FROM information_schema.columns WHERE table_name = p_table_name;
-    DECLARE
+    DECLARE counter INT DEFAULT 1;
+
+#     WHILE counter < JSON_LENGTH(vals) DO
+
 END //
 
 DELIMITER ;
